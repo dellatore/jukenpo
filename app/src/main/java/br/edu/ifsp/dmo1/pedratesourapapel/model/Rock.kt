88b848dc271b3b8ed8a7ec4e360ce69445823a00
Gabriel Dellatore <gabriel.dellatore@dbaonline.com.br>
2024-12-12 17:15:18 -0300

    versao 1.0 jukenpo usando kotlin

---
+++ app/src/main/java/br/edu/ifsp/dmo1/pedratesourapapel/model/Rock.kt
@@ -0,0 +1,3 @@
+package br.edu.ifsp.dmo1.pedratesourapapel.model
+
+object Rock : Weapon
---
+++ app/src/main/java/br/edu/ifsp/dmo1/pedratesourapapel/model/Weapon.kt
@@ -0,0 +1,5 @@
+package br.edu.ifsp.dmo1.pedratesourapapel.model
+
+import java.io.Serializable
+
+interface Weapon : Serializable
